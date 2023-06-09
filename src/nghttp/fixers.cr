184ea62480524445e90cb8001b29b642be9f121d
Brandon McGinty <git@bmcginty.us>
2023-06-09 17:45:32 -0400

    fixers: remove invalid absolute methods

--- src/nghttp/fixers.cr
+++ src/nghttp/fixers.cr
@@ -116,26 +116,6 @@ struct JSON::Any
   end   # def
 end     # struct
 
-class IO
-  def wait_readable?(t)
-    wait_readable(t)
-  end
-
-  def wait_readable?
-    wait_readable
-  end
-
-  def readable?
-    wait_readable
-  end
-end
-
-class Scheduler
-  def self.event_base
-    @@eb
-  end
-end
-
 struct HTTP::Headers
   def []=(key, value : Nil)
     @hash.delete key
@@ -199,42 +179,9 @@ class XML::Node
         elsif i[attrname] =~ /^[a-z]+:/
           # don't modify links that already have protocols and thus aren't relative
         else
-          i[attrname] = URI.parse(i[attrname]).normalize(bu).to_s
+          i[attrname] = bu.resolve(URI.parse(i[attrname])).to_s
         end # if
       end   # each
     end     # each attrname
   end       # def
 end         # class
-
-class URI
-  def self.normalize(url : String, base : String)
-    parse(url).normalize(base).to_s
-  end
-
-  def normalize(base : String)
-    normalize(URI.parse(base))
-  end
-
-  def normalize(base : URI)
-    dup.normalize! base
-  end
-
-  def normalize!(base : URI)
-    relative = @host ? true : false
-    unless @scheme
-      @scheme = base.scheme
-    end
-    unless @host
-      @host = base.host
-    end
-    unless @path
-      @path = base.path.to_s
-    end
-    unless @path.not_nil!.starts_with?("/")
-      bp = base.path ? base.path : ""
-      @path = bp.not_nil!.reverse.split("/", 2)[1].reverse + "/" + @path.not_nil!
-    end
-    normalize!
-    self
-  end
-end
