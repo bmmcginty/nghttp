d8f688cfd4e6899d4043056e636a0dd4cc7b4844
Brandon McGinty (codex) <git@bmcginty.us>
2026-07-22 15:03:13 -0400

    Cover HTTP2 request bodies

--- spec/nghttp_http2_protocol_spec.cr
+++ spec/nghttp_http2_protocol_spec.cr
@@ -12,4 +12,18 @@ describe NGHTTP::HTTP2Protocol do
       JSON.parse(resp.body)["path"].should eq "/get"
     end
   end
+
+  it "sends POST request bodies over HTTP/2 DATA frames" do
+    session = NGHTTP::Session.new
+    config = session.new_config
+    config.protocol = NGHTTP::HTTP2Protocol.new
+    headers = HTTP::Headers{"Content-Type" => "text/plain"}
+
+    session.post("#{SpecServers.http2_url}/echo", body: "hello h2", headers: headers, config: config) do |resp|
+      resp.http_version.should eq "2"
+      resp.status_code.should eq 200
+      resp.headers["content-type"].should eq "text/plain"
+      resp.body.should eq "hello h2"
+    end
+  end
 end
