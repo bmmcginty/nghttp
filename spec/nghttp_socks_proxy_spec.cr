3daa2d0fb2455992b805be7e8579640efde0de72
Brandon McGinty (codex) <git@bmcginty.us>
2026-07-23 17:00:52 -0400

    Cover HTTP2 through SOCKS4 proxies

--- spec/nghttp_socks_proxy_spec.cr
+++ spec/nghttp_socks_proxy_spec.cr
@@ -87,25 +87,102 @@ ensure
   end
 end
 
+private def local_socks4_proxy(&)
+  server = TCPServer.new("127.0.0.1", 0)
+  port = server.local_address.as(Socket::IPAddress).port
+  received = Channel(String).new(1)
+  done = Channel(Nil).new(1)
+
+  spawn do
+    client : TCPSocket? = nil
+    upstream : TCPSocket? = nil
+    begin
+      client = server.accept
+      version = client.read_byte
+      command = client.read_byte
+      raise "invalid SOCKS4 CONNECT" unless version == 4 && command == 1
+
+      port_bytes = Bytes.new(2)
+      client.read_fully(port_bytes)
+      target_port = (port_bytes[0].to_i << 8) | port_bytes[1].to_i
+
+      ip_bytes = Bytes.new(4)
+      client.read_fully(ip_bytes)
+      host = ip_bytes.join(".")
+
+      while byte = client.read_byte
+        break if byte == 0
+      end
+
+      received.send("#{host}:#{target_port}")
+
+      upstream = TCPSocket.new(host, target_port)
+      client.write(Bytes[0, 90, port_bytes[0], port_bytes[1], ip_bytes[0], ip_bytes[1], ip_bytes[2], ip_bytes[3]])
+      client.flush
+
+      pipe_done = Channel(Nil).new(2)
+      spawn do
+        copy_and_flush(client.not_nil!, upstream.not_nil!)
+      rescue
+      ensure
+        upstream.try(&.close)
+        pipe_done.send(nil)
+      end
+      spawn do
+        copy_and_flush(upstream.not_nil!, client.not_nil!)
+      rescue
+      ensure
+        client.try(&.close)
+        pipe_done.send(nil)
+      end
+      pipe_done.receive
+    ensure
+      client.try(&.close)
+      upstream.try(&.close)
+      done.send(nil)
+    end
+  end
+
+  yield "socks4://127.0.0.1:#{port}/", received
+ensure
+  server.close if server && !server.closed?
+  select
+  when done.not_nil!.receive
+  when timeout(1.second)
+  end
+end
+
+private def assert_http2_over_socks(proxy_url, received)
+  session = NGHTTP::Session.new
+  cfg = session.new_config
+  cfg.proxy = proxy_url
+  cfg.protocol = NGHTTP::HTTP2Protocol.new
+  cfg.verify = false
+  cfg.tries = 0
+  cfg.connect_timeout = 1.second
+  cfg.read_timeout = 1.second
+
+  session.get("#{SpecServers.http2_tls_url}/get", config: cfg) do |resp|
+    resp.http_version.should eq "2"
+    resp.status_code.should eq 200
+    JSON.parse(resp.body)["path"].should eq "/get"
+  end
+
+  received.receive.should match /^127\.0\.0\.1:\d+$/
+end
+
 describe NGHTTP::Socks5Proxy do
   it "negotiates HTTP/2 over TLS through SOCKS5 proxies" do
     local_socks5_proxy do |proxy_url, received|
-      session = NGHTTP::Session.new
-      cfg = session.new_config
-      cfg.proxy = proxy_url
-      cfg.protocol = NGHTTP::HTTP2Protocol.new
-      cfg.verify = false
-      cfg.tries = 0
-      cfg.connect_timeout = 1.second
-      cfg.read_timeout = 1.second
+      assert_http2_over_socks(proxy_url, received)
+    end
+  end
+end
 
-      session.get("#{SpecServers.http2_tls_url}/get", config: cfg) do |resp|
-        resp.http_version.should eq "2"
-        resp.status_code.should eq 200
-        JSON.parse(resp.body)["path"].should eq "/get"
-      end
-
-      received.receive.should match /^127\.0\.0\.1:\d+$/
+describe NGHTTP::Socks4Proxy do
+  it "negotiates HTTP/2 over TLS through SOCKS4 proxies" do
+    local_socks4_proxy do |proxy_url, received|
+      assert_http2_over_socks(proxy_url, received)
     end
   end
 end
